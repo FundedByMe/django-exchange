6c7f93e04bb1d7d59b602d60005784439f4cfd02
Sergey Khalymon <sergey.khalymon@fundedbyme.com>
2017-02-06 16:24:58 +0200

    Improve data migration script

--- exchange/migrations/0002_exchangerate_created_at.py
+++ exchange/migrations/0002_exchangerate_created_at.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import datetime
 
 from django.conf import settings
 from django.db import migrations, models
-import datetime
+from django.db.models import Q
 from django.utils.timezone import utc
 
 
 def remove_unused_currencies(apps, schema_editor):
     ExchangeRate = apps.get_model("exchange", "ExchangeRate")
     currencies = settings.CURRENCY_CHOICES_DICT.keys()
-    for rate in ExchangeRate.objects.all():
-        if rate.source.code not in currencies or rate.target.code not in currencies:
-            rate.delete()
+    rates = ExchangeRate.objects.filter(~Q(source__code__in=currencies) |
+                                        ~Q(target__code__in=currencies))
+    rates.delete()
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('exchange', '0001_initial'),
     ]
@@ -25,7 +25,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='exchangerate',
             name='created_at',
-            field=models.DateTimeField(default=datetime.datetime(2017, 1, 20, 14, 59, 20, 269556, tzinfo=utc), auto_now_add=True),
+            field=models.DateTimeField(default=datetime.datetime(2017, 1, 20, 14, 59, 20, 269556, tzinfo=utc),
+                                       auto_now_add=True),
             preserve_default=False,
         ),
         migrations.RunPython(remove_unused_currencies, lambda *x, **y: None)
